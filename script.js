494e1841a86d53bef94097a2e75b2ae0844196c8
cauchoix jade <jade.cauchoix@outlook.fr>
2023-01-08 01:39:34 +0100

    ajout du code

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-// console.log("tout fonctionne");
+const boutonAnimation = document.getElementById("bouton-animation");
+const animation = document.getElementById("animation");
 
-//const h1 = document.querySelector("h1")
-//h1.style.color = "green" 
-// document.querySelector("h1").style.color = "red"
-// style
-// button.style.color = "red";
-
-// class
-// body.classList.add("dark");
-
-
-// Selecteurs
- const body = document.querySelector("body");
- const h1 = document.querySelector("h1");
- const button = document.querySelector("button");
-
-
-// Listener
- button.addEventListener("click", function() {
- if (body.classList.contains("light")) {
-   body.classList.remove("light");
-   body.classList.add("dark");
-   button.innerText = "j'ai mal à la tête";
-   } else {
-    body.classList.add("light");
-    body.classList.remove("dark");
-    button.innerText = "j'ai mal aux yeux";
+boutonAnimation.addEventListener("click", function() {
+  if (animation.style.display === "none") {
+    animation.style.display = "block";
+  } else {
+    animation.style.display = "none";
   }
+});
 
- });
+  const boutonModeSombre = document.getElementById("bouton-mode-sombre");
+  const corps = document.body;
+
+  boutonModeSombre.addEventListener("click", function() {
+    if (corps.classList.contains("sombre")) {
+      corps.classList.remove("sombre");
+      boutonModeSombre.innerHTML = "🌚";
+    } else {
+      corps.classList.add("sombre");
+      boutonModeSombre.innerHTML = "☀️";
+    }
+  });
+  const button = document.querySelector("button");
+// const container = document.querySelector(".container");
+const cards = document.querySelectorAll('.cards');
+
+  console.log(button);
+  console.log(document.querySelector('.cards'));
+  console.log(document.querySelectorAll('.cards'));
+
+button.addEventListener("click", function () {
+
+    for (i = 0; i < 5; i++) {
+      cards[i].classList.toggle("active");
+      cards[i].style.transitionDelay = (0.25 * i) + "s";
+    }
+});
